294fa42711bdd3130bec6718c44d45638c4b0ab3
Fantros <krostotr@gmail.com>
2023-01-17 15:13:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
-function cookie() {}
+module.exports = {
+	serialize: function(name, val, options) {
+		var value = encodeURIComponent(val);
+		var str = name + '=' + value;
 
-cookie.parse = function(str) {
-    var obj = {};
+		if (null != options.maxAge) str += '; Max-Age=' + Math.floor(options.maxAge - 0);
+		if (options.domain) str += '; Domain=' + options.domain;
+		if (options.path) str += '; Path=' + options.path;
+		if (options.expires) str += '; Expires=' + options.expires.toUTCString();
+		if (options.httpOnly) str += '; HttpOnly';
+		if (options.secure) str += '; Secure';
 
-    var index = 0;
-    while (index < str.length) {
-        var eqIdx = str.indexOf('=', index);
-        if (eqIdx === -1) break;
+		if (options.priority) {
+			switch (typeof options.priority === 'string' ? options.priority.toLowerCase() : options.priority) {
+				case 'low': str += '; Priority=Low'; break;
+				case 'medium': str += '; Priority=Medium'; break;
+				case 'high': str += '; Priority=High'; break;
+			}
+		}
 
-        var endIdx = str.indexOf(';', index);
-        if (endIdx === -1) endIdx = str.length;
-        else if (endIdx < eqIdx) {
-            index = str.lastIndexOf(';', eqIdx - 1) + 1;
-            continue;
-        }
+		if (options.sameSite) {
+			switch (typeof options.sameSite === 'string' ? options.sameSite.toLowerCase() : options.sameSite) {
+				case true: str += '; SameSite=Strict'; break;
+				case 'lax': str += '; SameSite=Lax'; break;
+				case 'strict': str += '; SameSite=Strict'; break;
+				case 'none': str += '; SameSite=None'; break;
+			}
+		}
 
-        var key = str.slice(index, eqIdx).trim();
+		return str;
+	},
 
-        if (undefined === obj[key]) {
-            var val = str.slice(eqIdx + 1, endIdx).trim();
-            if (val.charCodeAt(0) === 0x22) val = val.slice(1, -1);
+	parse: function(str) {
+		var obj = {};
 
-            try {
-                obj[key] = val.indexOf('%') !== -1 ? decodeURIComponent(val) : val;
-            } catch (e) {
-                obj[key] = val;
-            }
-        }
+		var index = 0;
+		while (index < str.length) {
+			var eqIdx = str.indexOf('=', index);
+			if (eqIdx === -1) break;
 
-        index = endIdx + 1;
-    }
+			var endIdx = str.indexOf(';', index);
+			if (endIdx === -1) endIdx = str.length;
+			else if (endIdx < eqIdx) {
+				index = str.lastIndexOf(';', eqIdx - 1) + 1;
+				continue;
+			}
 
-    return obj;
-}
+			var key = str.slice(index, eqIdx).trim();
 
-cookie.serialize = function(name, val, options) {
-    var value = encodeURIComponent(val);
-    var str = name + '=' + value;
+			if (undefined === obj[key]) {
+				var val = str.slice(eqIdx + 1, endIdx).trim();
+				if (val.charCodeAt(0) === 0x22) val = val.slice(1, -1);
 
-    if (null != options.maxAge) str += '; Max-Age=' + Math.floor(options.maxAge - 0);
-    if (options.domain) str += '; Domain=' + options.domain;
-    if (options.path) str += '; Path=' + options.path;
-    if (options.expires) str += '; Expires=' + options.expires.toUTCString();
-    if (options.httpOnly) str += '; HttpOnly';
-    if (options.secure) str += '; Secure';
+				try {
+					obj[key] = val.indexOf('%') !== -1 ? decodeURIComponent(val) : val;
+				} catch (e) {
+					obj[key] = val;
+				}
+			}
 
-    if (options.priority) {
-        switch (typeof options.priority === 'string' ? options.priority.toLowerCase() : options.priority) {
-            case 'low': str += '; Priority=Low'; break;
-            case 'medium': str += '; Priority=Medium'; break;
-            case 'high': str += '; Priority=High'; break;
-        }
-    }
+			index = endIdx + 1;
+		}
 
-    if (options.sameSite) {
-        switch (typeof options.sameSite === 'string' ? options.sameSite.toLowerCase() : options.sameSite) {
-            case true: str += '; SameSite=Strict'; break;
-            case 'lax': str += '; SameSite=Lax'; break;
-            case 'strict': str += '; SameSite=Strict'; break;
-            case 'none': str += '; SameSite=None'; break;
-        }
-    }
-
-    return str;
-}
-
-module.exports = cookie;
+		return obj;
+	}
+};
